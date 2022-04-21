89ef14dbedbb31f209166f57e34a85315892689c
Dharam-shah <dharmendra11250@gmail.com>
2022-04-21 15:04:06 +0545

    implemented drf crud system using modelviewset

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
-from .views import test, TodoAppLC,TodoAppRUD
+from django import views
+from django.urls import path,include
+from todoapp import views
+from rest_framework.routers import DefaultRouter
+
+#creating router object
+router = DefaultRouter()
+#register TodoAppCRUD with router
+router.register('todoapi', views.TodoAppCRUD, basename='todoapi')
 
 urlpatterns = [
-    path("", test, name="test"),
-    path("todoapilc/", TodoAppLC.as_view(), name="todoapilc"),
-    path("todoapirud/<int:pk>", TodoAppRUD.as_view(), name="todoapirud"),
+    #path("", test, name="test"),
+    path('', include(router.urls)),
+    #path("todoapilc/", TodoAppLC.as_view(), name="todoapilc"),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,36 +1,42 @@
-import imp
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import TodoApp
 from .serializers import TodoAppSearilizers
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
+# from rest_framework.generics import GenericAPIView
+# from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
+from rest_framework import viewsets
 
 # Create your views here.
 
-def test(request):
-    return HttpResponse("test")
+# def test(request):
+#     return HttpResponse("test")
 
 """List and Create"""
-class TodoAppLC(GenericAPIView, ListModelMixin, CreateModelMixin):
+# class TodoAppLC(GenericAPIView, ListModelMixin, CreateModelMixin):
+#     queryset = TodoApp.objects.all()
+#     serializer_class = TodoAppSearilizers
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+"""Retrive, Update and Delete and Create"""
+# class TodoAppRUD(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+#     queryset = TodoApp.objects.all()
+#     serializer_class = TodoAppSearilizers
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+
+""""modelviewsets"""
+class TodoAppCRUD(viewsets.ModelViewSet):
     queryset = TodoApp.objects.all()
     serializer_class = TodoAppSearilizers
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-class TodoAppRUD(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
-    queryset = TodoApp.objects.all()
-    serializer_class = TodoAppSearilizers
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
